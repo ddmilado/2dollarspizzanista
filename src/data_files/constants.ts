857300edbc28bb7bf8118735715c8ae6fc81eb71
ddmilado <adebayodamilola@gmail.com>
2024-07-07 22:19:54 +0400

    mitey

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -2,11 +2,11 @@ import ogImageSrc from "@images/social.png";
 
 export const SITE = {
   title: "PIZZANISTA!",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  tagline: "PIZZANISTA Tuesdays ",
+  description: "PIZZANISTA Tuesdays NFT",
+  description_short: "PIZZANISTA Tuesdays NFT",
+  url: "https://pizzanista.com",
+  author: "Dami Adebayo",
 };
 
 export const SEO = {
@@ -33,7 +33,7 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  title: `${SITE.title}: : PIZZANISTA Tuesdays`,
+  description: "PIZZANISTA Tuesdays NFT",
   image: ogImageSrc,
 };
